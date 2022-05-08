da01e711c67ceb221b2fa40432b79d1daa3a832c
aref <arefabolhasani21@gmail.com>
2022-05-08 12:24:59 -0400

    responsive size fixed and menu icon button added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
 export default function Navbar() {
+  const [nav, setNav] = useState(false)
+  const handleClick = () => setNav(!nav);
+
   return (
-    <div className="w-screen h-[80px] bg-emerald-700 fixed drop-shadlow-lg">
+    <div className="w-screen h-[80px] bg-emerald-700 fixed drop-shadlow-lg z-10">
       <div className="px-2 flex justify-between items-center w-full h-full">
         <div className="flex items-center">
           <h1 className="text-3xl mr-4 font-bold sm:text-4xl text-green-50">
@@ -11,6 +14,7 @@ export default function Navbar() {
           </h1>
           <ul className="hidden md:flex text-white">
             <li>Home</li>
+            
             <li>About</li>
             <li>Support</li>
             <li>Platform</li>
@@ -21,12 +25,21 @@ export default function Navbar() {
           <button className="bg-transparent mr-3">Sign In</button>
           <button>Sign Up</button>
         </div>
-        <div className="md:hidden text-white">
-          <MenuIcon className="w-5" />
+        <div className="md:hidden text-white" onClick={handleClick}>
+          {!nav ? <MenuIcon className="w-5" /> : <XIcon className="w-5" />}
         </div>
       </div>
-      
-      
+      <ul className={!nav ? 'hidden' : "md:hidden absolute bg-emerald-700 w-full px-8"}>
+        <li className="border-b-2 text-white w-full">Home</li>
+        <li className="border-b-2 text-white w-full">About</li>
+        <li className="border-b-2 text-white w-full">Support</li>
+        <li className="border-b-2 text-white w-full">Platform</li>
+        <li className="border-b-2 text-white w-full">Pricing</li>
+        <div className="p-4 flex flex-col">
+          <button className="bg-transparent m-2">Sign In</button>
+          <button>Sign Up</button>
+        </div>
+      </ul>
     </div>
   );
 }
